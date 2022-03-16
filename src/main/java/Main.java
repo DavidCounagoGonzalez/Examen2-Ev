a9bfc2d18320b6cc214eb9874ea8798ebe5dadcc
David Counago Gonzalez <david.mouriscados@gmail.com>
2022-03-16 13:32:28 +0100

    Implementada la cración del token para el inicio del bot

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,10 @@
+import discord4j.core.DiscordClient;
+import discord4j.core.GatewayDiscordClient;
+
 public class Main {
+    public static void main (final String[]args ){
+        final String token =args[2];
+        final DiscordClient cliente = DiscordClient.create(token);
+        final GatewayDiscordClient puerto = cliente.login().block();
+    }
 }
