370f8ad02f4164aa99e7a469297cad211ff00d5d
David Counago Gonzalez <david.mouriscados@gmail.com>
2022-03-16 13:52:14 +0100

    Se comentó explicando las funciones de las partes del código

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,15 +6,18 @@ import discord4j.core.object.entity.channel.MessageChannel;
 
 public class Main {
     public static void main (final String[]args ){
-        final String token =args[2];
+        //Se registra el token  para poder usarse en el login en Discord
+        final String token =args[0];
         final DiscordClient cliente = DiscordClient.create(token);
         final GatewayDiscordClient puerto = cliente.login().block();
-
+            //Se crea una clase y se implementa al bot.
         puerto.on(MessageCreateEvent.class).subscribe(event -> {
+            //Y se crea el evento de recibir un mensaje
             final Message message = event.getMessage();
-            if ("!Ping".equals(message.getContent())) {
+            // se crea una igualdad con el mensaje recibido y buscando el canal en el que se escribió se devuelve el mensaje en ese canal.
+            if ("!Hambre".equals(message.getContent())) {
                 final MessageChannel channel = message.getChannel().block();
-                channel.createMessage("Pong!").block();
+                channel.createMessage("Toma un melocotón <3").block();
             }
         });
 
